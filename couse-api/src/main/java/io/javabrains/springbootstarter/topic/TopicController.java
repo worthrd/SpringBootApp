ea515345e786e4db9cd14a285dda27453d090733
Onur Arslan <onur.arslan@vodafone.com>
2019-01-03 09:42:57 +0300

    Request One Object

--- couse-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ couse-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,23 +1,30 @@
 package io.javabrains.springbootstarter.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() 
 	{
-	  return Arrays.asList(
-			  new Topic("spring", "Spring Framework", "Spring Framework Description"),
-			  new Topic("java", "Java Core Framework", "Java Framework Description"),
-			  new Topic("javascript", "Javascript Framework", "Javascript Framework Description")
-			  );
+	  return topicService.getAllTopics();
 			  
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		
+		return topicService.getTopic(id);
 	}
 
 }
---
+++ couse-api/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -0,0 +1,28 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+ private List<Topic> topics = 	Arrays.asList(
+			  new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			  new Topic("java", "Java Core Framework", "Java Framework Description"),
+			  new Topic("javascript", "Javascript Framework", "Javascript Framework Description")
+			  );
+ 
+ public List<Topic> getAllTopics(){
+	 
+	 return topics;
+	 
+ }
+ 
+ public Topic getTopic(String id) {
+	 return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+	 
+ }
+ 
+}
