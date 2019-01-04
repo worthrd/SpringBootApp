27c0e13f4250ef1ba950075ecf513b74820e913d
Onur Arslan <onur.arslan@vodafone.com>
2019-01-04 22:29:03 +0300

    DELETE and PUT  methods

--- couse-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ couse-api/src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -34,5 +34,16 @@ public class TopicController {
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 	}
+	
+	@RequestMapping(method=RequestMethod.PUT,value="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic,@PathVariable String id) {
+		topicService.updateTopic(id,topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE,value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
+	
 
 }
